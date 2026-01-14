b3771908d38c9213f9cce4f6b931ee5ff94338aa
José Novais <jlnovais@gmail.com>
2026-01-14 17:53:08 +0000

    fix: workflow

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,17 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MysqlDatabaseService } from './database/mysql-database.service';
 
 describe('AppController', () => {
   let appController: AppController;
+  let mysqlDatabaseService: jest.Mocked<MysqlDatabaseService>;
 
   beforeEach(async () => {
+    const mockMysqlDatabaseService = {
+      healthCheck: jest.fn(),
+    };
+
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [
+        AppService,
+        {
+          provide: MysqlDatabaseService,
+          useValue: mockMysqlDatabaseService,
+        },
+      ],
     }).compile();
 
     appController = app.get<AppController>(AppController);
+    mysqlDatabaseService = app.get(MysqlDatabaseService);
   });
 
   describe('root', () => {
